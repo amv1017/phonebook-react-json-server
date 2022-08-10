a29d04fceec3db06699e435a06de96d9e3549d27
Alexander <dr.ambercat@gmail.com>
2022-08-10 11:39:19 +0300

    db create

--- api/index.js
+++ api/index.js
@@ -12,6 +12,32 @@ app.get('/api', (req, res) => {
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
+app.get('/api/init-db-phones', (req, res) => {
+  const phones_data = [
+    {
+      id: 1,
+      name: 'Shoqe',
+      phone: '555-666-777',
+    },
+    {
+      id: 2,
+      name: 'Kryeminister',
+      phone: '898-010-989'
+    }
+  ]
+  const phones = db.addCollection('phones')
+  phones_data.map(phone => phones.insert(phone))
+  db.saveDatabase()
+})
+
+app.get('/phone/:id', (req, res) => {
+  db.loadDatabase({}, () => {
+    const phones = db.getCollection('phones')
+    const result = phones.findOne({ id: parseInt(req.params.id) })
+    res.status(200).json(result)
+  })
+})
+
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
